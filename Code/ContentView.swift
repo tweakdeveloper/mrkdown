84cfbe32837527a5343f8ed4c2feb60480252e0e
Nicola Clark <nicola@slottedspoon.dev>
2024-08-03 05:41:43 -0500

    now verifies state is the same

--- Code/ContentView.swift
+++ Code/ContentView.swift
@@ -8,14 +8,34 @@ struct ContentView: View {
   var body: some View {
     Button("sign in to tumblr") {
       Task {
-        let authURL = buildAuthURL()
+        let state = UUID().uuidString
+        let authURL = buildAuthURL(withState: state)
         do {
           let urlWithToken = try await webAuthenticationSession.authenticate(
             using: authURL,
             callbackURLScheme: "mrkdown",
             preferredBrowserSession: .shared
           )
-          print(urlWithToken.absoluteString)
+          let urlComponents = URLComponents(
+            url: urlWithToken,
+            resolvingAgainstBaseURL: false
+          )!
+          guard let stateParamIndex = urlComponents.queryItems?.firstIndex(
+            where: { param in
+              return param.name == "state"
+            }
+          ) else {
+            print("redirect did not include state param")
+            return
+          }
+          let receivedState = urlComponents.queryItems?[stateParamIndex].value
+          if(receivedState == state) {
+            print("login succeeded")
+          } else {
+            throw SignInError.stateDidNotMatch(receivedState ?? "unknown")
+          }
+        } catch SignInError.stateDidNotMatch(let receivedState) {
+          print("state did not match: got \(receivedState)")
         } catch {
           print("something bad happened: \(error)")
         }
--- Code/SignIn/SignIn.swift
+++ Code/SignIn/SignIn.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-func buildAuthURL() -> URL {
-  return URL(string: "https://mrkdown.slottedspoon.dev/init_auth_flow")!
+func buildAuthURL(withState state: String) -> URL {
+  var baseURL = URL(string: "https://mrkdown.slottedspoon.dev/init_auth_flow")!
+  baseURL.append(queryItems: [URLQueryItem(name: "state", value: state)])
+  return baseURL
+}
+
+enum SignInError: Error {
+  case stateDidNotMatch(String)
 }
