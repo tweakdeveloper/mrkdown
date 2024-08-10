0bd75a3b74bcf0ea870a06d490aa2513223baaa8
Nicola Clark <nicola@slottedspoon.dev>
2024-08-09 22:50:10 -0500

    added alert to confirm sending post without preview

--- Code/CreatePost/Screens/CreatePostView.swift
+++ Code/CreatePost/Screens/CreatePostView.swift
@@ -1,13 +1,39 @@
 import SwiftUI
 
 struct CreatePostView: View {
+  @State private var hasBeenPreviewed = false
   @State private var isPreviewing = false
   @State private var postText = "# howdy y'all"
-  
+  @State private var shouldShowSubmitConfirmation = false
+
+  private func showPreview() {
+    isPreviewing = true
+    hasBeenPreviewed = true
+  }
+
   var body: some View {
     NavigationStack {
       TextEditor(text: $postText)
+        .alert(
+          Text("Submit post without previewing?"),
+          isPresented: $shouldShowSubmitConfirmation
+        ) {
+          Button("No", role: .cancel) {
+            showPreview()
+          }
+          Button("Yes", role: .destructive) {
+            print("should submit post anyway")
+          }
+        } message: {
+          Text(
+            "You haven't yet previewed your latest modifications to the " +
+            "post. Are you sure you want to submit the post?"
+          )
+        }
         .navigationTitle("Create a Post")
+        .onChange(of: postText) { _ in
+          hasBeenPreviewed = false
+        }
         .sheet(isPresented: $isPreviewing) {
           NavigationStack {
             PreviewPostView(
@@ -19,10 +45,14 @@ struct CreatePostView: View {
         .toolbar {
           ToolbarItemGroup(placement: .topBarTrailing) {
             Button("Preview Post", systemImage: "doc") {
-              isPreviewing = true
+              showPreview()
             }
             Button("Submit Post", systemImage: "paperplane") {
-              print("should submit post")
+              if !hasBeenPreviewed {
+                shouldShowSubmitConfirmation = true
+              } else {
+                print("should submit post")
+              }
             }
           }
         }
