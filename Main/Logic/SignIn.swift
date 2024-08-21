0b378b090ee803b03be354626574403e6a3f1c37
Nicola Clark <nicola@slottedspoon.dev>
2024-08-20 22:01:45 -0500

    update sign in URL to reflect backend changes

--- Main/Logic/SignIn.swift
+++ Main/Logic/SignIn.swift
@@ -2,7 +2,10 @@ import Foundation
 
 func buildAuthURL(withState state: String) -> URL {
   var baseURL = URL(string: "https://mrkdown.slottedspoon.dev/init_auth_flow")!
-  baseURL.append(queryItems: [URLQueryItem(name: "state", value: state)])
+  baseURL.append(queryItems: [
+    URLQueryItem(name: "state", value: state),
+    URLQueryItem(name: "mobile", value: "true")
+  ])
   return baseURL
 }
 
