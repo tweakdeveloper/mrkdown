3239341425672a64435af1cb193ece51b57def74
Nicola Clark <nicola@slottedspoon.dev>
2024-08-27 17:48:47 -0500

    why weren't we using interpolation here???

--- Main/Logic/MainModel.swift
+++ Main/Logic/MainModel.swift
@@ -60,7 +60,7 @@ class MainModel: ObservableObject {
   }
 
   private func buildAuthURL(withState state: String) -> URL {
-    var baseURL = URL(string: "https://mrkdown.slottedspoon.dev/init_auth_flow")!
+    var baseURL = URL(string: "\(apiBase)/init_auth_flow")!
     baseURL.append(queryItems: [
       URLQueryItem(name: "state", value: state),
       URLQueryItem(name: "mobile", value: "true")
