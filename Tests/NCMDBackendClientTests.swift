228c0a53387d78713e64e39a90733463f454decb
Nicola Clark <nicola@slottedspoon.dev>
2024-09-15 16:07:36 -0500

    add test to ensure NCMDBackendClient.exchangeToken works

--- Tests/NCMDBackendClientTests.swift
+++ Tests/NCMDBackendClientTests.swift
@@ -3,6 +3,16 @@ import XCTest
 final class NCMDBackendClientTests: XCTestCase {
   private static let urlBase = "https://mrkdown.slottedspoon.dev"
 
+  var backendClient: NCMDBackendClient!
+
+  override func setUp() {
+    let configuration = URLSessionConfiguration.default
+    configuration.protocolClasses = [NCMDMockURLProtocol.self]
+    let session = URLSession(configuration: configuration)
+
+    backendClient = NCMDBackendClient(session: session)
+  }
+
   func testAuthURLIsCorrect() {
     let correctURLString = Self.urlBase + "/init_auth_flow?state=test&mobile=true"
     let correctURL = URL(string: correctURLString)
@@ -12,5 +22,30 @@ final class NCMDBackendClientTests: XCTestCase {
       actualURL,
       "generated URL did not match expected URL"
     )
+  }
+
+  func testExchangeCodeWithSuccessfulResponse() async throws {
+    let accessToken = "test_token"
+    let expiresIn = 3600
+    let tokenType = "bearer"
+    let scopes = "basic write"
+    let jsonString = """
+{
+  "access_token": "\(accessToken)",
+  "expires_in": \(expiresIn),
+  "token_type": "\(tokenType)",
+  "refresh_token": null,
+  "scope": "\(scopes)"
+}
+"""
+    let data = jsonString.data(using: .utf8)
+
+    NCMDMockURLProtocol.requestHandler = { request in
+      let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+      return (response, data)
+    }
+
+    let returnedToken = try await backendClient.exchangeCode("test_code")
+    XCTAssertEqual(returnedToken, accessToken, "NCMDBackendClient did not convert access token correctly")
   }
 }
---
+++ Tests/NCMDMockURLProtocol.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class NCMDMockURLProtocol: URLProtocol {
+  static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
+  override class func canInit(with request: URLRequest) -> Bool {
+    return true
+  }
+
+  override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+    return request
+  }
+
+  override func startLoading() {
+    guard let handler = NCMDMockURLProtocol.requestHandler else {
+      fatalError("handler is unavailable")
+    }
+    do {
+      let (response, data) = try handler(request)
+      client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+      if let data = data {
+        client?.urlProtocol(self, didLoad: data)
+      }
+      client?.urlProtocolDidFinishLoading(self)
+    } catch {
+      client?.urlProtocol(self, didFailWithError: error)
+    }
+  }
+}
